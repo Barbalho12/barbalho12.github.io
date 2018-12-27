8345852d4462275dc92c269fa0c6845f934f637e
Felipe Barbalho <felipebarbalho.95@gmail.com>
2018-12-27 11:18:07 -0300

    Initial information

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+// document.getElementById("my-name").onmouseenter = function(){displayText()};
+
+// function displayText() {
+    
+
+// }
+
+function mouseEnter() {
+  document.getElementById("my-name").style.color = "blue";
+}
+
+function mouseLeave() {
+  document.getElementById("my-name").style.color = "black";
+}
+
+document.getElementById("my-name").onmouseenter = function(){mouseEnter()};
+document.getElementById("my-name").onmouseleave = function(){mouseLeave()};
+
