afe39e80ff2413ebeacac72f1c0824e6c1c66bb8
Felipe Barbalho <felipebarbalho.95@gmail.com>
2019-01-12 21:39:02 -0300

    Changes in colors and layouts

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,26 @@
 // document.getElementById("my-name").onmouseenter = function(){mouseEnter()};
 // document.getElementById("my-name").onmouseleave = function(){mouseLeave()};
 
+
+// function randomColor(){
+//      color='rgba('+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+',0.5)';
+
+//      return color;
+// }
+
+
+// document.getElementsByClassName('box').style.backgroundColor = 'rgba('+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+',0.5)';
+
+
+function bgRandomColor() {
+  var elements = document.getElementsByClassName('box');
+  for( i=0 ; i < elements.length; i++) {
+  	r = Math.round(Math.random()*255);
+  	g = Math.round(Math.random()*255);
+  	b = Math.round(Math.random()*255);
+  	a = 0.1;
+    elements[i].style.backgroundColor = 'rgba('+r+','+g+','+b+','+a+')';
+  }
+}
+
+// bgRandomColor();
